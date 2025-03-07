8ea56504213811bdc7ea771a8fb0f77795b52cf1
Juree14 <104193498+Juree14@users.noreply.github.com>
2025-03-07 08:57:41 +0100

    dodal sem kosarico
    trenutno shranjuje samo v tabelo, vrjetno bova naredila bazo za kupce in dala podatke o kosarici v njo

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,8 +1,10 @@
-from bottle import Bottle, run, template, static_file
+from bottle import Bottle, run, template, static_file, request, redirect
 from model import Izdelek, Dobavitelj
 import os
 
 app = Bottle()
+
+kosarica = []  # Seznam za shranjevanje izdelkov v košarici
 
 @app.route('/')
 def zacetna_stran():
@@ -32,10 +34,11 @@ def prikazi_izdelke():
         </head>
         <body>
             <h1>Seznam izdelkov</h1>
+            <a href="/kosarica" style="position: absolute; top: 10px; right: 10px;"><button>Košarica</button></a>
             <div>
                 % for izdelek in izdelki:
                     <div style="display: inline-block; text-align: center; margin: 10px;">
-                        <a href="/dobavitelj/{{izdelek.dobavitelj_id}}">
+                        <a href="/dobavitelj/{{izdelek.dobavitelj_id}}/{{izdelek.id}}">
                             <img src="/slike_izdelkov/{{izdelek.ime.lower().replace(' ', '_')}}.png" alt="{{izdelek.ime}}" width="150">
                         </a>
                         <p>{{ izdelek.ime }}</p>
@@ -48,11 +51,14 @@ def prikazi_izdelke():
         </html>
     """, izdelki=izdelki)
 
-@app.route('/dobavitelj/<dobavitelj_id>')
-def prikazi_dobavitelja(dobavitelj_id):
+
+@app.route('/dobavitelj/<dobavitelj_id>/<izdelek_id>')
+def prikazi_dobavitelja(dobavitelj_id, izdelek_id):
     dobavitelj = Dobavitelj.najdi_po_id(dobavitelj_id)
-    if not dobavitelj:
-        return "<h1>Napaka: Dobavitelj ne obstaja</h1>"
+    izdelek = Izdelek.najdi_po_id(izdelek_id)
+    
+    if not dobavitelj or not izdelek:
+        return "<h1>Napaka: Dobavitelj ali izdelek ne obstajata</h1>"
 
     return template("""
         <!DOCTYPE html>
@@ -66,10 +72,42 @@ def prikazi_dobavitelja(dobavitelj_id):
             <p>Ime: {{dobavitelj.ime}}</p>
             <p>Naslov: {{dobavitelj.naslov}}</p>
             <p>Telefon: {{dobavitelj.telefonska_stevilka}}</p>
+            <form action="/dodaj_v_kosarico/{{izdelek.id}}" method="post">
+                <button type="submit">Dodaj v košarico</button>
+            </form>
             <a href="/izdelki"><button>Nazaj na izdelke</button></a>
         </body>
         </html>
-    """, dobavitelj=dobavitelj)
+    """, dobavitelj=dobavitelj, izdelek=izdelek)
+
+
+@app.route('/dodaj_v_kosarico/<izdelek_id>', method='POST')
+def dodaj_v_kosarico(izdelek_id):
+    izdelek = Izdelek.najdi_po_id(izdelek_id)
+    if izdelek:
+        kosarica.append(izdelek)
+    redirect('/kosarica')
+
+@app.route('/kosarica')
+def prikazi_kosarico():
+    return template("""
+        <!DOCTYPE html>
+        <html lang="sl">
+        <head>
+            <meta charset="UTF-8">
+            <title>Košarica</title>
+        </head>
+        <body>
+            <h1>Košarica</h1>
+            <ul>
+                % for izdelek in kosarica:
+                    <li>{{ izdelek.ime }} - {{ izdelek.cena }} €</li>
+                % end
+            </ul>
+            <a href="/izdelki"><button>Nazaj na izdelke</button></a>
+        </body>
+        </html>
+    """, kosarica=kosarica)
 
 @app.route('/slike_izdelkov/<filename>')
 def serviraj_sliko(filename):
